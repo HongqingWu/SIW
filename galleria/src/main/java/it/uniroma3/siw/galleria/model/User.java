743bea9c2ec18d7bb03f5f3c1189e56eaf96411c
11351 <11351@10.25.120.173>
2017-06-09 10:58:47 +0200

    home page

---
+++ galleria/src/main/java/it/uniroma3/siw/galleria/model/User.java
@@ -0,0 +1,33 @@
+package it.uniroma3.siw.galleria.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+@Entity
+public class User {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min=1)
+    private String firstName;
+
+    @NotNull
+    @Size(min=1)
+    private String lastName;
+    
+    @NotNull
+    
+    @Pattern(regexp=".+@.+\\.[a-z]+")
+    private String email;
+    
+    @NotNull
+    @Size(min=1)
+    private String password;
+    
+}
--- galleria/src/main/java/it/uniroma3/siw/galleria/model/UserDto.java
+++ galleria/src/main/java/it/uniroma3/siw/galleria/model/UserDto.java
@@ -1,32 +1,32 @@
 package it.uniroma3.siw.galleria.model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class UserDto {
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @NotNull
-    @Size(min=1)
-    private String firstName;
 
-    @NotNull
-    @Size(min=1)
-    private String lastName;
-    
-    @NotNull
-    
-    @Pattern(regexp=".+@.+\\.[a-z]+")
-    private String email;
-    
-    @NotNull
-    @Size(min=1)
-    private String password;
-    
+
+	@NotNull
+	@Size(min=1)
+	private String firstName;
+
+	@NotNull
+	@Size(min=1)
+	private String lastName;
+
+	@NotNull
+
+	@Pattern(regexp=".+@.+\\.[a-z]+")
+	private String email;
+
+	@NotNull
+	@Size(min=1)
+	private String password;
+	@NotNull
+	@Size(min=1)
+	private String matchingPassword;
 }
